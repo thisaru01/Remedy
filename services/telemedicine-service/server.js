2225897d039e3da1c947aa2788e89bfa39c334c0
Jayasanka Hirimuthugodage <hirimuthugodagejayasanka@gmail.com>
2026-03-22 18:20:02 +0530

    feat:initialize telemedicine Service

---
+++ services/telemedicine-service/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import connectDB from "./src/config/db.js";
+
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// MongoDB
+connectDB();
+
+// Routes
+
+
+// Test route
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "API is running 🚀" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
